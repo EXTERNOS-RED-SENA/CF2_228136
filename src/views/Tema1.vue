14719f60ddf32aca0740a09e9baec73f951b2a05
flizcanor <flizcanor@icloud.com>
2024-12-03 17:20:16 -0500

    vf

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,17 +2,153 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Modelamiento avanzado de datos
+    
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5(data-aos="flip-up" data-aos-duration="1000")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema1/1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 El modelamiento avanzado de datos constituye la base de la construcción de sistemas de información modernos, especialmente aquellos destinados a soportar aplicaciones de inteligencia artificial. Este capítulo explora las técnicas y metodologías para diseñar e implementar las estructuras de datos que almacenen información de manera eficiente, y que también soporten las complejas necesidades analíticas de los sistemas de IA actuales. Abordaremos desde las reglas de negocio fundamentales hasta los principios de diseño de bases de datos, estableciendo una base sólida para la gestión efectiva de datos en entornos empresariales modernos.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_1_1 1.1 Reglas de negocio y metodologías
+    
+    p(data-aos="fade-right") El modelamiento avanzado de datos representa un paso obligado en la gestión moderna de información, especialmente cuando se orienta hacia aplicaciones de inteligencia artificial. Este proceso va más allá del simple diseño de estructuras de almacenamiento, incorporando las reglas de negocio que definen cómo la organización opera y toma decisiones.
+
+    .row.mt-4
+      .col-lg(data-aos="fade-right")
+        .p-4(style="background-color:#ECF2FF;")
+          .row
+            .col-md-auto.d-none.d-md-block
+              figure
+                img(src="@/assets/curso/tema1/2.svg")
+            .col-md
+              p.mb-0 Las reglas de negocio son las directrices que gobiernan aspectos del negocio y determinan cómo los datos deben ser capturados, almacenados, transformados y utilizados. Estas reglas pueden variar desde simples validaciones (como "la edad de un empleado debe ser mayor a 18 años") hasta complejas interrelaciones entre diferentes aspectos del negocio (como "un cliente premium debe haber realizado compras por más de $10,000 en los últimos 6 meses y tener un historial de pagos impecable").
+        p.mt-4.mb-0 Las metodologías de modelamiento actuales tienen un enfoque iterativo y colaborativo, que les permite a los expertos en datos trabajar estrechamente con los expertos del dominio para asegurar que el modelo final refleje adecuadamente tanto los requisitos técnicos como las necesidades del negocio.
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema1/3.png")
+
+
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_1_2 1.2 Normalización y diseño de bases de datos
+    
+    .row
+      .col-lg(style="place-self:center;" data-aos="fade-right")
+        p La normalización es un proceso fundamental en el diseño de bases de datos que busca eliminar redundancias y dependencias problemáticas. Para comprender mejor este proceso, se deben conocer las diferentes formas normales y su impacto en el diseño de bases de datos. Cada forma normal representa un nivel incremental de organización y optimización de la estructura de datos.
+        p.mb-0.fw-bold La siguiente tabla expone una visión general de las principales formas normales, sus objetivos y consideraciones clave:
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-left" style="place-self:center;")
+        figure
+          img(src="@/assets/curso/tema1/9.png")
+    
+    .titulo-sexto.color-acento-contenido.mt-5(data-aos="fade-down")
+      h5 Tabla 1.
+      span Formas de normalización en las bases de datos
+    .tabla-a.color-acento-contenido(data-aos="fade-up")
+      table
+        caption.fw-normal #[strong Fuente.] OIT, 2024.
+        thead
+          tr.text-white.text-center(style="background-color:#03A9F4;")
+            th Forma normal
+            th Objetivo
+            th Beneficios
+            th Consideraciones
+        tbody.text-center
+          tr.bg-white
+            td 1FN
+            td Eliminar grupos repetitivos
+            td Atomicidad de datos
+            td Puede aumentar el número de tablas
+          tr.bg-white
+            td 2FN
+            td Eliminar dependencias parciales
+            td Mejor integridad de datos
+            td Requiere identificar dependencias funcionales
+          tr.bg-white
+            td 3FN
+            td Eliminar dependencias transitivas
+            td Reduce redundancia
+            td Puede afectar el rendimiento de consultas
+          tr.bg-white
+            td BCNF
+            td Eliminar anomalías restantes
+            td Máxima normalización práctica
+            td Complejidad aumentada
+          tr.bg-white
+            td 4FN
+            td Manejar dependencias multivaluadas
+            td Mejor manejo de relaciones M:N
+            td Raramente necesaria
+
+    p.mt-4(data-aos="fade-right") Esta progresión de formas normales ilustra cómo el proceso de normalización evoluciona desde conceptos básicos hasta consideraciones más avanzadas. Sin embargo, es importante notar que no siempre es necesario o deseable alcanzar los niveles más altos de normalización. La decisión de hasta qué nivel normalizar debe basarse en un análisis cuidadoso de los requisitos específicos del sistema, considerando factores como el rendimiento de las consultas, la naturaleza de los datos y las necesidades del negocio.
+
+    .cont_1_2.mt-4
+    
+      Separador
+
+      .titulo-segundo(data-aos="flip-up")
+        h2#t_1_3 1.3 Principios ACID (acrónimo en inglés de atomicidad, consistencia, aislamiento y durabilidad)
+      
+      p(data-aos="fade-right") Los principios ACID constituyen la base de la integridad transaccional en sistemas de bases de datos. Estos principios son especialmente determinantes en entornos donde la precisión y la consistencia de los datos son fundamentales para las operaciones del negocio.
+
+      .row.mt-4.justify-content-center
+        .col-lg.col-md-9.mb-4.mb-lg-0(data-aos="zoom-in" data-aos-duration="800")
+          .cont_1_3.h-100.p-4
+            figure.mb-3
+              img(src="@/assets/curso/tema1/12.svg")
+            p.fw-bold.mb-3 Atomicidad
+            p Asegura que una transacción se complete en su totalidad o no se realice en absoluto. Por ejemplo, en una transferencia bancaria, tanto el débito de una cuenta como el crédito en otra deben completarse exitosamente, o ninguna operación debe realizarse.
+        .col-lg.col-md-9.mb-4.mb-lg-0(data-aos="zoom-in" data-aos-duration="800")
+          .cont_1_3.h-100.p-4
+            figure.mb-3
+              img(src="@/assets/curso/tema1/13.svg")
+            p.fw-bold.mb-3 Consistencia
+            p Garantiza que una transacción lleve la base de datos de un estado válido a otro igualmente válido. Esto significa que todas las reglas de integridad definidas deben cumplirse antes y después de cada transacción.
+        .col-lg.col-md-9.mb-4.mb-lg-0(data-aos="zoom-in" data-aos-duration="800")
+          .cont_1_3.h-100.p-4
+            figure.mb-3
+              img(src="@/assets/curso/tema1/14.svg")
+            p.fw-bold.mb-3 Aislamiento
+            p Previene que transacciones concurrentes interfieran entre sí. Cada transacción debe ejecutarse como si fuera la única operación siendo realizada en el sistema, aunque en realidad múltiples transacciones pueden estar ejecutándose simultáneamente.
+        .col-lg.col-md-9.mb-4.mb-lg-0(data-aos="zoom-in" data-aos-duration="800")
+          .cont_1_3.h-100.p-4
+            figure.mb-3
+              img(src="@/assets/curso/tema1/15.svg")
+            p.fw-bold.mb-3 Durabilidad
+            p  Asegura que una vez una transacción se ha completado, sus efectos son permanentes y sobrevivirán a cualquier falla subsecuente del sistema. Esto típicamente se logra a través de logs de transacciones y mecanismos de recuperación.
+    
+    .row.mt-4.mt-lg-5
+      .col-lg(data-aos="fade-right")
+        .p-4.h-100.d-flex.align-content-center.flex-wrap(style="background-color:#FDF9F3;")
+          p.mb-0 En el contexto de la IA, estos principios ACID adquieren una nueva dimensión de importancia. Los modelos de IA dependen claramente de la calidad y consistencia de los datos de entrenamiento, y cualquier violación de estos principios podría resultar en modelos sesgados o poco confiables. Por ejemplo, si los datos de entrenamiento se recopilan durante estados inconsistentes de la base de datos, los patrones aprendidos por el modelo podrían no reflejar la realidad del negocio.
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema1/16.png")
+
+    p.mt-5(data-aos="fade-right") El desafío actual en el modelamiento avanzado de datos radica en encontrar el equilibrio adecuado entre estos principios tradicionales y las necesidades emergentes de sistemas de IA, que a menudo requieren flexibilidad y velocidad en el acceso a los datos.
+
+
+
       
 </template>
 
 <script>
+import SlyderA from '../bootstrap/SlyderA.vue'
+import AcordionA from '../bootstrap/AcordionA.vue'
 export default {
   name: 'Tema1',
-  components: {},
+  components: {
+    SlyderA,
+    AcordionA,
+  },
   data: () => ({
     // variables de vue
   }),
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,16 +2,154 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Análisis exploratorio de datos
+    
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5(data-aos="flip-up" data-aos-duration="1000")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema3/1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 El análisis exploratorio de datos (EDA, por sus siglas en inglés) constituye una etapa fundamental en el proceso de comprensión y preparación de datos para modelos de inteligencia artificial. Esta fase inicial de investigación ayuda a los analistas a comprender las características fundamentales de los conjuntos de datos, identificar patrones significativos y detectar anomalías que podrían afectar análisis posteriores. A diferencia de los métodos estadísticos confirmatorios, el EDA adopta un enfoque más flexible y orientado al descubrimiento, permitiendo que los datos revelen sus secretos de manera orgánica.
+      
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_3_1 3.1 Estadística descriptiva e inferencial
+    
+    p(data-aos="fade-up") La estadística descriptiva proporciona el conjunto de herramientas fundamental para comenzar cualquier análisis exploratorio. A través de medidas de tendencia central, dispersión y forma, los analistas pueden obtener una primera aproximación a la naturaleza de sus datos. Sin embargo, el verdadero poder de la estadística descriptiva radica en su capacidad para revelar aspectos sutiles de los datos que podrían pasar desapercibidos en un examen superficial.
+
+    .row.mt-4
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-right")
+        figure
+          img(src="@/assets/curso/tema3/2.png")
+      .col-lg(data-aos="fade-left")
+        .p-4(style="background-color:#ECF2FF;")
+          .row
+            .col-md-auto.d-none.d-md-block
+              figure
+                img(src="@/assets/curso/tema3/3.svg")
+            .col-md
+              p.mb-0 Las medidas de tendencia central, como la media, mediana y moda, ofrecen diferentes perspectivas sobre el centro de los datos. Por ejemplo, en distribuciones altamente sesgadas, la diferencia significativa entre la media y la mediana puede revelar la presencia de valores extremos que requieren atención especial. La dispersión de los datos, medida a través de la varianza, desviación estándar y rango intercuartílico, proporciona información clave sobre la variabilidad y la confiabilidad de las mediciones.
+        p.mt-4.mb-0 Por su parte, la estadística inferencial permite extender las conclusiones obtenidas de una muestra a la población general. Este proceso requiere una comprensión profunda de conceptos como intervalos de confianza, pruebas de hipótesis y significancia estadística. En el contexto del análisis exploratorio, las técnicas inferenciales ayudan a validar patrones observados y a determinar si las relaciones descubiertas son estadísticamente significativas.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_3_2 3.2 Métodos de análisis univariable y multivariable
+    
+    p(data-aos="fade-up") La estadística descriptiva proporciona el conjunto de herramientas fundamental para comenzar cualquier análisis exploratorio. A través de medidas de tendencia central, dispersión y forma, los analistas pueden obtener una primera aproximación a la naturaleza de sus datos. Sin embargo, el verdadero poder de la estadística descriptiva radica en su capacidad para revelar aspectos sutiles de los datos que podrían pasar desapercibidos en un examen superficial.
+
+    .row.mt-4
+      .col-lg(data-aos="fade-right")
+        .p-4.mt-4(style="background-color:#ECF2FF;")
+          ol.lista-ol--cuadro.mb-0
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span.fw-bold 1
+              span Distribución de frecuencias y visualizaciones básicas
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span.fw-bold 2
+              span Detección de valores atípicos y anomalías
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span.fw-bold 3
+              span Análisis de valores faltantes
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span.fw-bold 4
+              span Evaluación de la normalidad
+            li.d-flex.mb-0
+              .lista-ol--cuadro__vineta
+                span.fw-bold 5
+              span Identificación de patrones temporales o secuenciales
+        p.mt-4.mb-0 El análisis multivariable, por su parte, examina las relaciones entre múltiples variables simultáneamente. Este enfoque es particularmente relevante en el contexto de la inteligencia artificial, donde los modelos frecuentemente deben procesar numerosas variables interrelacionadas. #[strong La complejidad de estas relaciones puede apreciarse mejor a través de la siguiente tabla comparativa:]
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema3/4.png")
+    
+    .titulo-sexto.color-acento-contenido.mt-4(data-aos="fade-down")
+      h5 Tabla 2.
+      span Tipos de análisis: técnicas, aplicaciones y consideraciones clave
+    .tabla-a.color-acento-contenido(data-aos="fade-up")
+      table
+        caption.fw-normal #[strong Fuente.] OIT, 2024.
+        thead
+          tr.text-white.text-center(style="background-color:#03A9F4;")
+            th Tipo de Análisis
+            th Técnicas Principales
+            th Aplicaciones
+            th Consideraciones clave
+        tbody.text-center
+          tr.bg-white
+            td Bivariado
+            td Correlación de Pearson, Chi-cuadrado
+            td Relaciones entre pares de variables
+            td Asunciones de linealidad
+          tr.bg-white
+            td Regresión múltiple
+            td Mínimos cuadrados, Ridge, Lasso
+            td Predicción de variables continuas
+            td Multicolinealidad
+          tr.bg-white
+            td Análisis factorial
+            td PCA, Factor Analysis
+            td Reducción de dimensionalidad
+            td Interpretabilidad de factores
+          tr.bg-white
+            td Análisis de clústeres
+            td K-means, jerárquico
+            td Segmentación de datos
+            td Selección de número de grupos
+          tr.bg-white
+            td Análisis discriminante
+            td LDA, QDA
+            td Clasificación supervisada
+            td Separabilidad de clases
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_3_3 3.3 Técnicas de visualización
+    
+    p(data-aos="fade-up") La visualización de datos representa una herramienta indispensable en el análisis exploratorio, transformando números abstractos en representaciones visuales intuitivas que facilitan la identificación de patrones, tendencias y anomalías. Las técnicas de visualización modernas van más allá de los gráficos básicos, incorporando elementos interactivos y múltiples dimensiones de información en una sola representación.
+
+    .row.mt-4
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-right")
+        figure
+          img(src="@/assets/curso/tema3/5.png")
+      .col-lg(data-aos="fade-left")
+        .p-4(style="background-color:#F7EEFE;")
+          .row
+            .col-md-auto.d-none.d-md-block
+              figure
+                img(src="@/assets/curso/tema3/6.svg")
+            .col-md
+              p.mb-0 La selección de la técnica de visualización adecuada depende tanto de la naturaleza de los datos como del objetivo del análisis. Por ejemplo, los diagramas de dispersión resultan invaluables para examinar relaciones entre variables continuas, mientras que los gráficos de calor pueden revelar patrones complejos en matrices de correlación. Los gráficos de caja (box plots) combinan múltiples aspectos de la distribución de datos, mostrando simultáneamente la mediana, cuartiles y valores atípicos.
+        p.mt-4.mb-0 En el contexto del big data y la inteligencia artificial, las técnicas de visualización han evolucionado para manejar volúmenes masivos de datos. Las visualizaciones interactivas permiten a los analistas explorar diferentes niveles de detalle, mientras que las técnicas de reducción de dimensionalidad como t-SNE y UMAP facilitan la visualización de datos altamente dimensionales en espacios bidimensionales o tridimensionales.
+    
+    .row.mt-4
+      .col-lg(data-aos="fade-right")
+        p La efectividad de una visualización no solo depende de su precisión técnica, sino también de su capacidad para comunicar información de manera clara y convincente. Los principios de diseño visual, como el uso apropiado del color, la gestión del espacio y la jerarquía visual, desempeñan un papel destacado en la creación de visualizaciones que sean tanto informativas como accesibles
+        p.mb-0.fw-bold El análisis exploratorio de datos, con sus múltiples facetas y técnicas, es una fase crítica en cualquier proyecto de análisis de datos o inteligencia artificial. La combinación de estadística rigurosa, análisis multivariable y visualización efectiva proporciona los cimientos necesarios para construcción de modelos robustos y confiables.
+      .col-lg-auto.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema3/7.png")
+
+
 
 </template>
 
 <script>
+import LineaTiempoD from '../bootstrap/LineaTiempoD.vue'
 export default {
   name: 'Tema3',
+  components: {
+    LineaTiempoD,
+  },
   data: () => ({
     // variables de vue
   }),
